bb3d03836e1897a89b6675b50543ddbfdfb7f234
Steve Streeting <steve@stevestreeting.com>
2022-01-24 16:16:52 +0000

    Mark Waypoint component as needing saving

--- Source/SUQS/Public/SuqsWaypointActor.h
+++ Source/SUQS/Public/SuqsWaypointActor.h
@@ -7,12 +7,13 @@
 class UWidgetComponent;
 class USuqsWaypointComponent;
 /// Waypoint actor which brings together the waypoint component and a UMG visual
+/// NOTE: if using a save/load system like SPUD you'll need a subclass with a post-restore hook to call UpdateWaypointVisibility()
 UCLASS(Blueprintable, ClassGroup=(SUQS))
 class SUQS_API ASuqsWaypointActor : public AActor
 {
 	GENERATED_BODY()
 protected:
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, SaveGame)
 	USuqsWaypointComponent* WaypointComponent;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
